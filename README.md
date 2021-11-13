f21cca59381147cd6ade67e1fd6c5918f7e2571d
aakritsubedi <aakritsubedi9@gmail.com>
2021-11-13 18:17:16 +0545

    ETL: Extract data from the different file source

---
+++ README.md
@@ -0,0 +1,92 @@
+# Steps
+
+- Get the data from the different source
+- Create raw table schemas
+- Add data into the raw table
+- Archive all the data from raw tables
+- Create the required table schema
+- Transform the data as requirements
+- Load the data into database
+
+## Get the data from the different source
+
+Using python
+we can download the csv, json or xml file to our server and place them in the `data` directory.
+
+## Create raw table schemas
+
+We have 2different sets of data
+
+- Employees
+- Timesheets
+
+The raw structure of Employees and timesheets from the data are:
+
+- Employees
+
+```
+employee_id
+first_name
+last_name
+department_id
+department_name
+manager_employee_id
+employee_role
+salary
+hire_date
+terminated_date
+terminated_reason
+dob
+fte
+location
+```
+
+- Timesheets
+
+```
+employee_id
+cost_center
+punch_in_time
+punch_out_time
+punch_apply_date
+hours_worked
+paycode
+```
+
+#### SQL for creating Raw table schemas
+
+```sql
+-- Creating raw_employees table
+CREATE TABLE raw_employees (
+  employee_id VARCHAR(255),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  department_id VARCHAR(255),
+  department_name VARCHAR(255),
+  manager_employee_id VARCHAR(255),
+  employee_role VARCHAR(255),
+  salary VARCHAR(255),
+  hire_date VARCHAR(255),
+  terminated_date VARCHAR(255),
+  terminated_reason VARCHAR(255),
+  dob VARCHAR(255),
+  fte VARCHAR(255),
+  location VARCHAR(255),
+);
+
+-- Creating raw_timesheets table
+CREATE TABLE raw_timesheets(
+  employee_id VARCHAR(255),
+  cost_center VARCHAR(255),
+  punch_in_time VARCHAR(255),
+  punch_out_time VARCHAR(255),
+  punch_apply_date VARCHAR(255),
+  hours_worked VARCHAR(255),
+  paycode VARCHAR(255),
+);
+```
+
+We use all VARCHAR(255) because ... 
+
+# Add data into the raw table
+ 
---
+++ src/pipeline/01_extract_raw_tables.py
@@ -0,0 +1,110 @@
+import json 
+import psycopg2
+import xmltodict
+
+def connect():
+  return psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="postgres",
+    password="postgres",
+    database="etl"
+  )
+
+def readFromCSV(filepath):
+  data = []
+  i=0
+  with open(filepath, 'r') as file:
+    for line in file:
+      # Skipping headers
+      if i == 0:
+        i += 1
+        continue
+
+      data.append(line)
+
+  return data
+
+def readFromJSON(filepath):
+  with open(filepath, 'r') as file:
+    data = file.read()
+
+    return data
+
+
+def readFromXML(filepath):
+  data = []
+  with open(filepath, 'r') as file:
+    data = xmltodict.parse(file.read())
+    data = json.dumps(data)
+    data = json.loads(data)
+    data = data['EmployeeList']['Employee']
+    
+  return data
+
+def readFromFile(format, filepath):
+  if(format == 'csv'):
+    return readFromCSV(filepath)
+  elif (format == 'json'):
+    return readFromJSON(filepath)
+  elif (format == 'xml'):
+    return readFromXML(filepath)
+  else:
+    print('Invalid Format')
+
+def extract_employee_data(format, filepath, cur, con):
+  employee_sql="""
+    INSERT INTO raw_employees (employee_id,first_name,last_name,department_id,department_name,manager_employee_id,employee_role,salary,hire_date,terminated_date,terminated_reason,dob,fte,location) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+  """
+
+  # Inserting Into raw employees table
+  employees = readFromFile(format, filepath)
+  for employee in employees:
+    if format == 'csv':
+      employee = employee.strip().split(',')
+    else:
+      employee = list(employee.values())
+    cur.execute(employee_sql, employee)
+    con.commit()
+
+def extract_timesheet_data(format, filepath, cur, con):
+  timesheets_sql="""
+    INSERT INTO raw_timesheets(employee_id,cost_center,punch_in_time,punch_out_time,punch_apply_date,hours_worked,paycode) VALUES(%s, %s, %s, %s, %s, %s, %s)
+  """
+  
+  # Inserting Into raw timesheets table
+  timesheets = readFromFile(format,  filepath)
+
+  for timesheet in timesheets:
+    if format == 'csv':
+      timesheet = timesheet.strip().split(',')
+    else:
+      timesheet = list(timesheet.values())
+    
+    cur.execute(timesheets_sql, timesheet)
+    con.commit()
+
+def truncate_table(table_name, cur, con):
+  sql = "TRUNCATE TABLE " + table_name
+  cur.execute(sql)
+  con.commit()
+
+def main():
+  con = connect()
+  cur = con.cursor()
+
+  # Truncate Tables
+  truncate_table('raw_employees', cur, con)
+  truncate_table('raw_timesheets', cur, con)
+
+  # Insert into raw tables
+  extract_employee_data('xml', 'data/employee_2021_08_01.xml', cur, con)
+  extract_timesheet_data('csv', 'data/timesheet_2021_05_23.csv', cur, con)
+  extract_timesheet_data('csv', 'data/timesheet_2021_06_23.csv', cur, con)
+  extract_timesheet_data('csv', 'data/timesheet_2021_07_24.csv', cur, con)
+
+  cur.close()
+  con.close()
+
+if __name__ == "__main__":
+  main()
